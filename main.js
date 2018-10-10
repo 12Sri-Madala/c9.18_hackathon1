4996bfd77483b71a4c2f672b71a7c6b7c38bf638
Andrew Moser <andrewsmoser@gmail.com>
2018-10-10 13:28:07 -0700

    created dynamic gameboard - need css for this

--- main.js
+++ main.js
@@ -2,5 +2,29 @@ $(document).ready(initializeApp);
 
 function initializeApp(){
     // insert functions here to run when DOM is loaded
+    create_board();
+}
 
+var game_board = [
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0]
+];
+
+function create_board(){
+    for(var rowIndex =0; rowIndex < game_board.length; rowIndex++){
+        var row = $("<div>").addClass("row");
+        for(var colIndex = 0; colIndex < game_board.length; colIndex ++){
+            var square = $("<div>").addClass("square").attr("row", rowIndex).attr("col", colIndex);
+            row.append(square);
+        }
+
+        //add class name between quotes. Maybe .game_area?
+        $("").append(row);
+    }
 }
